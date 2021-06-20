a1fa9a3f04a94e9520ce2f6cd19e4008c25c02a3
githubcratos <edwardcampbell2014@gmail.com>
2021-06-20 11:05:57 +0100

    worked on drill home page (View comment for more detail)

--- xword_data/views.py
+++ xword_data/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Puzzle, Entry, Clue
+from django.contrib import messages
+# from django.urls import reverse
 
 
 # Create your views here.
 def drill(request):
-    context={}
-    return render(request, 'xword/answer.html', context)
+    # Testing Reverse Routing.
+    # found = reverse('xword-drill') 
+    # print(found)
+
+    # set error to false
+    is_error = False
+
+
+    if request.method == 'POST':
+        answer = request.POST.get('answer')
+        clue_id = request.POST.get('clue_id')
+        clue = Clue.objects.get(pk=clue_id)
+        if clue.entry.entry_text == answer.upper():
+            messages.success(request, "Correct!!!")
+            return redirect('xword-answer', pk=clue.pk)
+        is_error = True
+    else:
+        clue = Clue.objects.order_by('?').first()
+    context={
+        'rand_clue': clue,
+        'clue_id':clue.id,
+        'is_error': is_error
+    }
+    return render(request, 'xword/drill.html', context)
 
 
 def answer(request, pk):
